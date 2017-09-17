a182c1188840d621833a5ed1111e43ec38f18c94
lechuckcaptain <lechuckcaptain@gmail.com>
2017-09-17 23:10:00 +0200

    Add draft project structure and basic functionalities

---
+++ api/api.go
@@ -0,0 +1,88 @@
+package api
+
+import (
+	"log"
+	mattermost "github.com/mattermost/platform/model"
+	"time"
+)
+
+func GetClient(url string, username string, password string) *mattermost.Client {
+
+	client := mattermost.NewClient(url)
+	r, e := client.Login(username, password)
+	if e != nil {
+		log.Fatal("Couldn't login: ", e)
+	}
+	log.Printf("Client logged in to '%v'. Auth Token: %s.", url, client.AuthToken)
+	user := r.Data.(*mattermost.User)
+	log.Printf("User information: %s", user.ToJson())
+	return client
+}
+
+func PrintTeams(teamMap map[string]*mattermost.Team) {
+	log.Printf("Found %v teams:", len(teamMap))
+	for teamId, team := range teamMap {
+		log.Printf("* %s -> %s", teamId, team.DisplayName)
+	}
+}
+
+func PrintChannels(channelList *mattermost.ChannelList) {
+	log.Printf("Found %v channels:", len(*channelList))
+	for _, channel := range *channelList {
+		log.Printf("* %s -> %s", channel.Id, channel.DisplayName)
+	}
+}
+
+func PrintPosts(posts *mattermost.PostList) {
+	log.Print("Posts:")
+	for _, post := range posts.Posts {
+		log.Printf("%s -> '%s' by %v @ %v", post.Id, post.Message, post.UserId, post.CreateAt)
+	}
+}
+
+func GetTeams(client *mattermost.Client) map[string]*mattermost.Team {
+	r, e := client.GetAllTeams()
+	if e != nil {
+		log.Fatal("Couldn't get team map: ", e)
+	}
+	teamMap := r.Data.(map[string]*mattermost.Team)
+	return teamMap
+}
+
+func GetChannels(client *mattermost.Client) *mattermost.ChannelList {
+	r, e := client.GetChannels("")
+	if e != nil {
+		log.Fatal("Couldn't get channels: ", e)
+	}
+	return r.Data.(*mattermost.ChannelList)
+}
+
+func GetPosts(client *mattermost.Client, channelId string, start int, max int) *mattermost.PostList {
+	r, e := client.GetPosts(channelId, start, max, "")
+	if e != nil {
+		log.Fatal("Couldn't get posts: ", e)
+	}
+	posts := r.Data.(*mattermost.PostList)
+	return posts
+}
+
+func GetTodaysPosts(client *mattermost.Client, channelId string) *mattermost.PostList {
+	n := time.Now()
+	today := time.Date(n.Year(),n.Month(),n.Day(),0,0,0,0, n.Location()).Unix()
+	r, e := client.GetPostsSince(channelId, today)
+	if e != nil {
+		log.Fatal("Couldn't get posts: ", e)
+	}
+	posts := r.Data.(*mattermost.PostList)
+	return posts
+}
+
+func GetUsersFromPosts(posts *mattermost.PostList) {
+
+
+	//var m map[string]
+	//
+	//for _, post := range posts.Posts {
+	//
+	//}
+}
---
+++ teamcheck/teamcheck.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"log"
+	"os"
+	mattermost "github.com/mattermost/platform/model"
+	"math/big"
+	"github.com/lechuckcaptain/mattermost/api"
+)
+
+func main() {
+
+	args := os.Args
+	log.Printf("Started TeamCheck\n")
+	log.Printf("Recevived args: '%v'\n", args)
+
+	url := "http://127.0.0.1:8065"
+	username := "test"
+	password := "testtest"
+	channelToCheck := "Town Square"
+
+	if client := api.GetClient(url, username, password); client != nil {
+
+		teams := api.GetTeams(client)
+		api.PrintTeams(teams)
+
+		for _, team := range teams {
+
+			log.Printf("Selecting Team: %v (%v)",team.DisplayName, team.Id)
+			client.SetTeamId(team.Id)
+			channels := api.GetChannels(client)
+			api.PrintChannels(channels)
+
+			r,e := client.GetTeamMembers(team.Id,0, big.MaxExp)
+			if e != nil {
+				log.Fatal("Couldn't get team members")
+			}
+
+			log.Printf(r.RequestId)
+			members := r.Data.([]*mattermost.TeamMember)
+			log.Printf("Found %v team members",len(members))
+			usersIds := make([] string,len(members))
+			for i, member := range members {
+				log.Printf("* %s", member.UserId)
+				usersIds[i] = member.UserId
+			}
+
+			log.Print("Users ids: ", usersIds)
+			r1 ,e := client.GetProfilesByIds(usersIds)
+			if e != nil {
+				log.Fatal("Couldn't get user profiles")
+			}
+			users := r1.Data.(map[string]*mattermost.User)
+			log.Printf("Found %v profiles",len(users))
+			for _, user := range users {
+				log.Printf("* %s %s", user.Id, user.Username )
+			}
+
+			for _, channel := range *channels {
+
+				if channelToCheck == channel.DisplayName {
+
+					log.Printf("Checking todays post for channel %v",channel.DisplayName)
+					posts := api.GetTodaysPosts(client,channel.Id)
+					api.PrintPosts(posts)
+				}
+			}
+		}
+	}
+}
