ea1aff70255a6959f43fa44a978d3654c43777d1
lechuckcaptain <lechuckcaptain@gmail.com>
2017-11-11 18:54:51 +0100

    Migrated to API v4

--- teamcheck/teamcheck.go
+++ teamcheck/teamcheck.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	mattermost "github.com/mattermost/platform/model"
 	"github.com/lechuckcaptain/mattermost-utils/api"
 	"log"
 	"os"
@@ -24,56 +25,50 @@ func run(url string, username string, password string, channelToCheck string) {
 	log.Printf("Started TeamCheck\n")
 	log.Printf("Recevived args: '%v'\n", args)
 
-	if client := api.GetClient(url, username, password); client != nil {
+	client := mattermost.NewAPIv4Client(url)
 
-		//teams := api.GetTeams(client)
-		//if len(teams) == 0 {
-		//	log.Fatal("No teams available for current user")
-		//}
-		//
-		//api.PrintTeams(teams)
-		//
-		//for _, team := range teams {
-		//
-		//	log.Printf("Selecting Team: %v (%v)", team.DisplayName, team.Id)
-		//	client.SetTeamId(team.Id)
-		//	channels := api.GetChannels(client)
-		//	api.PrintChannels(channels)
-		//
-		//	r, e := client.GetTeamMembers(team.Id, 0, big.MaxExp)
-		//	if e != nil {
-		//		log.Fatal("Couldn't get team members")
-		//	}
-		//
-		//	log.Printf(r.RequestId)
-		//	members := r.Data.([]*mattermost.TeamMember)
-		//	log.Printf("Found %v team members", len(members))
-		//	usersIds := make([]string, len(members))
-		//	for i, member := range members {
-		//		log.Printf("* %s", member.UserId)
-		//		usersIds[i] = member.UserId
-		//	}
-		//
-		//	log.Print("Users ids: ", usersIds)
-		//	r1, e := client.GetProfilesByIds(usersIds)
-		//	if e != nil {
-		//		log.Fatal("Couldn't get user profiles")
-		//	}
-		//	users := r1.Data.(map[string]*mattermost.User)
-		//	log.Printf("Found %v profiles", len(users))
-		//	for _, user := range users {
-		//		log.Printf("* %s %s", user.Id, user.Username)
-		//	}
-		//
-		//	for _, channel := range *channels {
-		//
-		//		if channelToCheck == channel.DisplayName {
-		//
-		//			log.Printf("Checking todays post for channel %v", channel.DisplayName)
-		//			posts := api.GetTodaysPosts(client, channel.Id)
-		//			api.PrintPosts(posts)
-		//		}
-		//	}
-		//}
+	api.Login(client, username, password)
+
+	teams := api.GetTeams(client)
+	if len(teams) == 0 {
+		log.Fatal("No teams available for current user")
+	}
+
+	PrintTeams(teams)
+
+	for _, team := range teams {
+
+		log.Printf("Using Team: %v (%v)", team.DisplayName, team.Id)
+		channels := api.GetChannels(client,team.Id)
+		PrintChannels(channels)
+
+		teamMembers := api.GetTeamMemmbers(client,team.Id)
+		log.Printf("Found %v team members", len(teamMembers))
+		usersIds := make([]string, len(teamMembers))
+		for i, member := range teamMembers {
+			log.Printf("* %s", member.UserId)
+			usersIds[i] = member.UserId
+		}
+
+		log.Print("Users ids: ", usersIds)
+		users, response := client.GetUsersByIds(usersIds)
+		if response.Error != nil {
+			log.Fatal("Couldn't get user profiles: ", response.Error)
+		}
+
+		log.Printf("Found %v profiles", len(users))
+		for _, user := range users {
+			log.Printf("* %s %s", user.Id, user.Username)
+		}
+
+		for _, channel := range channels {
+
+			if channelToCheck == channel.DisplayName {
+
+				log.Printf("Checking todays post for channel %v", channel.DisplayName)
+				posts := api.GetTodaysPosts(client, channel.Id)
+				PrintPosts(posts)
+			}
+		}
 	}
 }
